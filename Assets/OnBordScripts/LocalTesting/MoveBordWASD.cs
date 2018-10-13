782db993cdaabc1b70eb017a55c8955eb825962b
DaviisLV <abols.davis@gmail.com>
2018-10-13 12:16:25 +0300

    first commit

---
+++ Assets/OnBordScripts/LocalTesting/MoveBordWASD.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveBordWASD : MonoBehaviour {
+    
+    private enum Selection { lvl1, lvl2, lvl3 };
+    [SerializeField]
+    private Selection _levelSelected = Selection.lvl1;
+    public Transform Bord;
+    private Vector3 _basePoz;
+// Use this for initialization
+    void Start () {
+        _basePoz = Bord.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.W))
+            Move(1);
+        if (Input.GetKeyDown(KeyCode.S))
+            Move(2);
+        if (Input.GetKeyDown(KeyCode.A))
+            Move(3);
+        if (Input.GetKeyDown(KeyCode.D))
+            Move(4);
+    }
+    private void Move(int index)
+    {
+        Vector3 _basePoz = Bord.transform.position;
+        int _moveRange = 0;
+        switch (_levelSelected)
+        {
+            case Selection.lvl1:
+                _moveRange = 2;
+                break;
+            case Selection.lvl2:
+                _moveRange = 4;
+                break;
+            case Selection.lvl3:
+                _moveRange = 6;
+                break;
+        }
+
+        switch (index)
+        {
+            case 1:
+                Bord.position = 
+                    new Vector3(Bord.position.x, Bord.position.y, Bord.position.z + _moveRange);
+                break;
+            case 2:
+                Bord.position =
+                    new Vector3(Bord.position.x, Bord.position.y, Bord.position.z - _moveRange);
+                break;
+            case 3:
+                Bord.position =
+                    new Vector3(Bord.position.x - _moveRange, Bord.position.y, Bord.position.z);
+                break;
+            case 4:
+                Bord.position =
+                    new Vector3(Bord.position.x + _moveRange, Bord.position.y, Bord.position.z);
+                break;
+
+        }
+        StartCoroutine(BeckToBasePoz());
+    }
+
+    IEnumerator BeckToBasePoz()
+    {
+        yield return new WaitForSeconds(0.3f);
+        Bord.position = _basePoz;
+    }
+}
---
+++ Assets/OnBordScripts/LocalTesting/TrackGenerator.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrackGenerator : MonoBehaviour {
+
+    public GameObject TargetObX;
+    public GameObject TargetObZ;
+    public Transform Bord;
+
+    private Vector3 _x1StartPosition;
+    private Vector3 _x2StartPosition;
+    private Vector3 _z1StartPosition;
+    private Vector3 _z2StartPosition;
+    private int _distance = 10;
+    private float _moveRage = 2f;
+    private float _speed = 2f;
+	// Use this for initialization
+	void Start () {
+        _z1StartPosition = new Vector3(Bord.position.x + _distance, Bord.position.y, Bord.position.z - _moveRage);
+        _z2StartPosition = new Vector3(Bord.position.x + _distance, Bord.position.y, Bord.position.z + _moveRage);
+        _x1StartPosition = new Vector3(Bord.position.x - _moveRage, Bord.position.y, Bord.position.z + _distance);
+        _x2StartPosition = new Vector3(Bord.position.x + _moveRage, Bord.position.y, Bord.position.z + _distance);
+
+        InvokeRepeating("CreateTarget",0f, _speed);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+      
+    }
+
+
+    public void CreateTarget()
+    {
+        int rand = Random.Range(0, 4);
+        switch (rand)
+        {
+            case 0:
+                Instantiate(TargetObX, _x1StartPosition, Quaternion.identity);
+                break;
+            case 1:
+                Instantiate(TargetObX, _x2StartPosition, Quaternion.identity);
+                break;
+            case 2:
+                Instantiate(TargetObZ, _z1StartPosition, Quaternion.identity);
+                break;
+            case 3:
+                Instantiate(TargetObZ, _z2StartPosition, Quaternion.identity);
+                break;
+        }
+    }
+}
